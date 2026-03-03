3578b2b6ce87b0798275588530dacd82c8b56c1e
maximebaumard-alt <maximebaumard@gmail.com>
2026-03-03 21:28:49 +0100

    Add files via upload

---
+++ netlify:functions:strava-token.js
@@ -0,0 +1,48 @@
+exports.handler = async function(event) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  const { code } = JSON.parse(event.body || '{}');
+  if (!code) {
+    return { statusCode: 400, body: JSON.stringify({ error: 'Missing code' }) };
+  }
+
+  try {
+    const response = await fetch('https://www.strava.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        client_id: '207792',
+        client_secret: 'e3a31dd34315546c960d6391c7700d2aff344a3e',
+        code: code,
+        grant_type: 'authorization_code'
+      })
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
